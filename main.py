6d2d1f70cc690f368d7912e8eae38be910311438
Roman Potemin <poteminr@gmail.com>
2022-05-05 19:50:57 +0700

    remove restart command

--- main.py
+++ main.py
@@ -3,21 +3,10 @@ from scripts.registration import init_user_group, conv_handler_pick_university, 
 from scripts.view_assignment import conv_handler_view_assignment, view_assignment_for_specific_date, view_page
 from scripts.add_assignment import conv_handler_add_assignment_by_hand, conv_handler_add_assignment_during_seminar
 import logging
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext, ConversationHandler, CallbackQueryHandler
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-
-def restart(update: Update, _: CallbackContext):
-    user = update.message.from_user
-
-    logger.info("User %s canceled the conversation.", user.first_name)
-
-    update.message.reply_text(text="Бот перезагружен!")
-
-    return ConversationHandler.END
 
 
 def main():
@@ -28,6 +17,7 @@ def main():
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("group", init_user_group))
+
     dispatcher.add_handler(CallbackQueryHandler(change_page_with_information, pattern='^bot_info#'))
 
     dispatcher.add_handler(conv_handler_pick_university)
@@ -38,12 +28,10 @@ def main():
 
     dispatcher.add_handler(conv_handler_add_assignment_during_seminar)
 
-    dispatcher.add_handler(CommandHandler("restart", restart))
-
     dispatcher.add_handler(CommandHandler("info", info))
 
     dispatcher.add_handler(CommandHandler("view", view_assignment_for_specific_date))
-    updater.dispatcher.add_handler(CallbackQueryHandler(view_page, pattern='^assignment#'))
+    dispatcher.add_handler(CallbackQueryHandler(view_page, pattern='^assignment#'))
 
     updater.start_polling()
 
